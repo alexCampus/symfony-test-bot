d5daaacca2c47b1bc609c9b8b1f4778c74511eb5
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-05-29 15:41:10 +0200

    test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,14 +10,15 @@ class DefaultController extends Controller
     public function index(Request $request)
     {
         $content = json_decode($request->getContent(), true);
-        var_dump('REQUEST',$content["queryResult"]);
+//        var_dump('REQUEST',$content["queryResult"]);
         $response = array(
             'fulfillmentText' => 'Hello',
             'fulfillmentMessages'=> array(
                 array(
                     'text' => array(
                         'text' => array(
-                            'Tu vis à ' . $content["queryResult"]["parameters"]["ville"]
+                            'Tu vis à ' . $content["queryResult"]["parameters"]["ville"],
+                            'Tu vis à turlutuuu' . $content["queryResult"]["parameters"]["ville"]
                         )
                     )
                 )
@@ -32,7 +33,12 @@ class DefaultController extends Controller
 
     private function webhook($data)
     {
-
+        if ($data['intent']['displayName'] === 'City') {
+            $response = 'Tu vis à ' . $data["parameters"]["ville"];
+        } else {
+            $response = "Malheureusement je ne connais pas cette ville....";
+        }
+        return $response;
     }
 
 }
