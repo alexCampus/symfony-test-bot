2c5cf0349803b4906b89225e9e7a123719bd813f
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-05-29 12:34:27 +0200

    routing

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index(Request $request)
     {
         $content = json_decode($request->getContent(), true);
-        $data = json_decode($response->getBody()->getContents(), true);
         var_dump('REQUEST',$content);
-        var_dump('RESPONSE',$data);
         die;
         return $this->render('index.html.twig');
     }
