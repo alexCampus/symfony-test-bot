b8c116308a399cec02d466b4b80691a7148f9fa0
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-05-29 15:53:45 +0200

    test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,16 +10,14 @@ class DefaultController extends Controller
     public function index(Request $request)
     {
         $content = json_decode($request->getContent(), true);
+        $test = $this-><$this->webhook($content['queryResult']);
 //        var_dump('REQUEST',$content["queryResult"]);
         $response = array(
             'fulfillmentText' => 'Hello',
             'fulfillmentMessages'=> array(
                 array(
                     'text' => array(
-                        'text' => array(
-                            'Tu vis à ' . $content["queryResult"]["parameters"]["ville"],
-                            'Tu vis à turlutuuu' . $content["queryResult"]["parameters"]["ville"]
-                        )
+                        'text' => $test
                     )
                 )
 
@@ -34,11 +32,38 @@ class DefaultController extends Controller
     private function webhook($data)
     {
         if ($data['intent']['displayName'] === 'City') {
-            $response = 'Tu vis à ' . $data["parameters"]["ville"];
+            $city = json_decode(
+                        file_get_contents("https://geo.api.gouv.fr/communes?nom=" . $this->skip_accents($data["parameters"]["ville"]) . "&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre")
+            );
+            if (count($city) > 0) {
+                foreach ($city as $c) {
+                    if ($c->nom === $data["parameters"]["ville"]) {
+                        $response = array(
+                            'Tu vis à ' . $c->nom,
+                            'Cette ville est dans le département : ' . $c->codeDepartement,
+                            'Elle a une population de : ' . number_format($c->population)
+                        );
+                    } else {
+                        $response = ["Malheureusement je ne connais pas cette ville...."];
+                    }
+                }
+            }
+
         } else {
-            $response = "Malheureusement je ne connais pas cette ville....";
+            $response = ["J'ai mal compris votre demande."];
         }
         return $response;
+    }
+
+    private function skip_accents( $str, $charset='utf-8' ) {
+
+        $str = htmlentities( $str, ENT_NOQUOTES, $charset );
+
+        $str = preg_replace( '#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str );
+        $str = preg_replace( '#&([A-za-z]{2})(?:lig);#', '\1', $str );
+        $str = preg_replace( '#&[^;]+;#', '', $str );
+
+        return $str;
     }
 
 }
