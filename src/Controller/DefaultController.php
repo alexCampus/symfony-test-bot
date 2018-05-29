a1f0c730093d843c78bcaade8752d8644ed0fb61
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-05-29 12:32:15 +0200

    request

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,12 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, Response $response)
     {
-        var_dump($request);die;
+        $content = json_decode($request->getContent(), true);
+        $data = json_decode($response->getBody()->getContents(), true);
+        var_dump('REQUEST',$content);
+        var_dump('RESPONSE',$data);
+        die;
         return $this->render('index.html.twig');
     }
 
