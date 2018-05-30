5eff32c51ea1992955215e97d819409585c5647b
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-05-30 16:37:15 +0200

    refacto Service

--- src/Service/TraitementResponse.php
+++ src/Service/TraitementResponse.php
@@ -47,7 +47,7 @@ class TraitementResponse
                     $date =
 //                    var_dump($responseData['rain']['3h']);
                     $response = ['Le '. date('d M Y',strtotime($time[0])) . ' à ' . date('G:i',strtotime($responseData['dt_txt'])) . ', il devrait faire une température de : ' . ceil($responseData['main']['temp']) . ' degrés.',
-                        "La probalité de pluie dans les 3h est de : " . ceil($responseData['rain']['3h']*100) . '%',
+                        "il devrait tombé dans en 3h : " . ceil($responseData['rain']['3h']) . 'mm de pluie',
                         'http://openweathermap.org/img/w/' . $responseData['weather'][0]['icon'] . '.png'];
                     break;
             }
