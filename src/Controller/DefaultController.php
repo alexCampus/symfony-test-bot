db1788a71c8b8d0273e286c165ba332027d387dd
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-05-30 15:10:59 +0200

    refacto Service

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -22,9 +22,11 @@ class DefaultController extends Controller
     {
         $content      = json_decode($request->getContent(), true);
         $responseText = $this->webhook->getIntent($content['queryResult']);
-        $response     = ['fulfillmentText' => 'Hello', 'fulfillmentMessages' => [['card' => ['subtitle' => 'test'],['text' => ['text' => $responseText]]
-
-        ]];
+        $response     = ['fulfillmentText' => 'Hello', 'fulfillmentMessages' => [
+            [
+                'card' => ['subtitle' => 'test'],
+                'text' => ['text' => $responseText]
+            ]]];
 
         return $this->json($response);
 
