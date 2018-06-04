c18bc248a60bd5fa6ee2ef2d68d7ee22272f57ac
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-06-04 09:40:32 +0200

    sending image api

--- src/Service/TraitementResponse.php
+++ src/Service/TraitementResponse.php
@@ -47,21 +47,21 @@ class TraitementResponse
                 case 'meteo':
                     $responseData = $this->meteoInfo->getMeteo($city);
 
+                    $time = preg_split('/ /',$responseData['dt_txt']);
+                    $response = ['Le '. date('d M Y',strtotime($time[0])) . ' à ' . date('G:i',strtotime($responseData['dt_txt'])) . ', il devrait faire une température de : ' . ceil($responseData['main']['temp']) . ' degrés.',
+                        "il devrait tombé dans en 3h : " . ceil($responseData['rain']['3h']) . 'mm de pluie'
+                        ];
+
                     // Test message direct slack
                     $client = new Client();
-                    $res = $client->request('POST', 'https://slack.com/api/chat.postMessage', [
+                    $res = $client->request('POST', 'https://slack.com/api/files.upload', [
                         'form_params' => [
                             'token'   => 'xoxp-371098517505-371098517713-376014330567-066539b85aa89593ff9fd045740ab3fa',
                             'channel' => 'général',
-                            'text'    => 'hello poulet'
+                            'file'    => 'http://openweathermap.org/img/w/' . $responseData['weather'][0]['icon'] . '.png'
                         ]
                     ]);
                     // Test message direct slack
-
-                    $time = preg_split('/ /',$responseData['dt_txt']);
-                    $response = ['Le '. date('d M Y',strtotime($time[0])) . ' à ' . date('G:i',strtotime($responseData['dt_txt'])) . ', il devrait faire une température de : ' . ceil($responseData['main']['temp']) . ' degrés.',
-                        "il devrait tombé dans en 3h : " . ceil($responseData['rain']['3h']) . 'mm de pluie',
-                        'http://openweathermap.org/img/w/' . $responseData['weather'][0]['icon'] . '.png'];
                     break;
             }
         } else {
