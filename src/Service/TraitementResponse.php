d3ee89c3ae5ff683689935c40fb6ee67dee990b8
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-06-04 09:51:48 +0200

    sending image api

--- src/Service/TraitementResponse.php
+++ src/Service/TraitementResponse.php
@@ -46,12 +46,6 @@ class TraitementResponse
                     break;
                 case 'meteo':
                     $responseData = $this->meteoInfo->getMeteo($city);
-
-                    $time = preg_split('/ /',$responseData['dt_txt']);
-                    $response = ['Le '. date('d M Y',strtotime($time[0])) . ' à ' . date('G:i',strtotime($responseData['dt_txt'])) . ', il devrait faire une température de : ' . ceil($responseData['main']['temp']) . ' degrés.',
-                        "il devrait tombé dans en 3h : " . ceil($responseData['rain']['3h']) . 'mm de pluie',
-                        'http://openweathermap.org/img/w/' . $responseData['weather'][0]['icon'] . '.png'];
-
                     // Test message direct slack
                     $client = new Client();
                     $res = $client->request('POST', 'https://slack.com/api/files.upload', [
@@ -65,6 +59,12 @@ class TraitementResponse
                         ]
                     ]);
                     // Test message direct slack
+                    $time = preg_split('/ /',$responseData['dt_txt']);
+                    $response = ['Le '. date('d M Y',strtotime($time[0])) . ' à ' . date('G:i',strtotime($responseData['dt_txt'])) . ', il devrait faire une température de : ' . ceil($responseData['main']['temp']) . ' degrés.',
+                        "il devrait tombé dans en 3h : " . ceil($responseData['rain']['3h']) . 'mm de pluie',
+                        'http://openweathermap.org/img/w/' . $responseData['weather'][0]['icon'] . '.png'];
+
+
                     break;
             }
         } else {
